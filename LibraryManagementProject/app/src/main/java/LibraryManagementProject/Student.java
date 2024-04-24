655191851802e691c0f922594fac793f4cb957bd
emmaxsolo <emmasolo22@gmail.com>
2024-04-24 17:00:54 -0400

    New Updates

---
+++ LibraryManagementProject/app/src/main/java/LibraryManagementProject/Student.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LibraryManagementProject;
+
+import java.util.List;
+
+/**
+ *
+ * @author emmas
+ */
+public class Student {
+    int studentID;
+    String name;
+    int contactNumber;
+    
+    public List<Book> searchBookByTitle(String title){
+        return null;
+    }
+}
